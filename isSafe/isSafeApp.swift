de2c1d47ea80958cb201a84396db01a9c888e0b1
Nik <hrezolitaz@gmail.com>
2023-08-17 09:06:42 +0600

    Initial Commit

---
+++ isSafe/isSafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  isSafeApp.swift
+//  isSafe
+//
+//  Created by Nikita on 17/8/23.
+//
+
+import SwiftUI
+
+@main
+struct isSafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
